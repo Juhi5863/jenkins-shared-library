2f9e75f6015be3bbafd89d2b732e5791fda8d36b
Juhi Choudhary <115893427+Juhi5863@users.noreply.github.com>
2025-04-09 17:19:01 +0530

    Update commonBuild.groovy

--- vars/commonBuild.groovy
+++ vars/commonBuild.groovy
@@ -1,19 +1,9 @@
-// vars/commonBuild.groovy
-
-def call(Map config = [:]) {
+def call() {
     stage('Checkout') {
         checkout scm
     }
-
     stage('Unit Tests') {
         echo 'Running unit tests...'
-        sh './gradlew test'
-    }
-
-    stage('Deploy') {
-        def env = config.env ?: 'dev'
-        echo "Deploying to environment: ${env}"
-        // You can replace this with actual deployment commands
-        // Example: sh "./deploy.sh ${env}"
+        // sh './gradlew test'  ← Correct comment syntax
     }
 }
